6c85fa015057ba6baf941c2a4ab659f77792cd88
patarr19NEO <yurapatarr19@yandex.ru>
2025-08-21 18:41:23 +0300

    some changes in Header.jsx

--- password_manager/src/App.jsx
+++ password_manager/src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css"
+import Header from './components/Header/Header.jsx'
 
 function App() {
   return (
-      <div className="hello">
-        <header>
-            <div className="logo">
-                <img src="./assets/icon-key/icon-key.png" alt="logo-key"/>
-                <h1>Password Manager</h1>
-            </div>
-        </header>
-      </div>
+      <Header />
   )
 }
 
---
+++ password_manager/src/Components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <header>
+                <div className="logo">
+                    <img src="./assets/icon-key/icon-key.png" alt="logo-key"/>
+                    <h1>Password Manager</h1>
+                </div>
+                <nav>
+                    <ul>
+                        <li>About</li>
+                        <li>Contacts</li>
+                        <li>Prices</li>
+                    </ul>
+                </nav>
+                <a className="buy-button">Try NOW for FREE</a>
+            </header>
+        </div>
+    )
+}
+
+export default Header
