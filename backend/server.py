3095ce5c0491cb1a1e54f1938bc0c7fe2ca44bf1
patarr19NEO <yurapatarr19@yandex.ru>
2025-08-22 23:48:41 +0300

    added db.json and funcs to work with it. added logic has a user exists already or not

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os, json
 
 app = Flask(__name__)
 CORS(app)
 
+db = "db.json"
+
+def save_data_to_db(data):
+    with open(db, "w") as file:
+        json.dump(data, file, indent=4)
+
+def load_data_from_db():
+    if os.path.exists(db):
+        with open(db, "r") as file:
+            return json.load(file)
+    return {"users": {}}
+
 @app.route("/api/users", methods=["GET", "POST"])
 def users():
     try:
-        data = request.get_json()
-        print(f"Got data: {data}")
-        return jsonify({"message": "Successfully fetched data!", "gotten_data": data}), 200
+        gotten_data = request.get_json()
+        print(f"Got data: {gotten_data}")
+
+        data = load_data_from_db()
+        if data["users"]["email"]:
+            return jsonify({"message": "Successfully fetched data!", "code": "user exists", "gotten_data": data}), 200
+
+        data["users"]["email"] = {gotten_data["email"]}
+        
     except Exception as e:
         return jsonify({"error": f"Error: {e}"}), 500
 
